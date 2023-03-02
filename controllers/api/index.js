a4ac44961ba644b3be62f7ccc4bb6f8af0b3e810
Ben L <veloswim@gmail.com>
2023-03-02 12:31:14 -0800

    added file structure for api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const apiRoutes = require("./api");
-router.use("/api", apiRoutes);
-
 router.get("/", async (req, res) => {
-  res.json({ msg: "Wrong Route!" });
+  res.json({ msg: "API route" });
 });
 
 module.exports = router;
