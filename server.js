bd27bbcc12d752245286cb9247db6643c6a28002
Alex <alexhallinthecloud@icloud.com>
2023-03-02 14:20:37 -0800

    socket server

--- server.js
+++ server.js
@@ -8,6 +8,19 @@ const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//==============================================================
+// Socket.io for chat feature
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+//==============================================================
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
