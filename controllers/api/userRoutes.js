7d4c9d5a0caa22c052c8d0979fbd0cae5b2062b1
Ben L <veloswim@gmail.com>
2023-03-04 13:47:24 -0800

    required bcrypt, jwt in userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const { User, Cat, Room } = require("../../models");
 
 router.get("/", async (req, res) => {
