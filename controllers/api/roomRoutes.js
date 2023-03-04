8a790cb12a79fc24eeafcc857bf16856fab74514
Ben L <veloswim@gmail.com>
2023-03-04 14:59:39 -0800

    added room name to models and added get all rooms and one room routes

--- controllers/api/roomRoutes.js
+++ controllers/api/roomRoutes.js
@@ -1,4 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const { User, Room, Cat } = require("../../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const allRooms = await Room.findAll();
+    res.json(allRooms);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ msg: "An error occurred in the get all rooms route", err });
+  }
+});
+
+router.get("/:roomcode", async (req, res) => {
+  try {
+    const foundRoom = await Room.findOne({
+      where: {
+        code: req.params.roomcode,
+      },
+    });
+    if (!foundRoom) {
+      return res.status(404).json({ msg: "No such room" });
+    }
+    res.json(foundRoom);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ msg: "An error occurred in the get one room route", err });
+  }
+});
 
 module.exports = router;
