ac503274ee3364e66a8f4f2ab65410487a0f5200
Ben L <veloswim@gmail.com>
2023-03-04 14:48:08 -0800

    created file and imported for room routes in api folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const userRoutes = require("./userRoutes");
 router.use("/users", userRoutes);
 
+const roomRoutes = require("./roomRoutes");
+router.use("/rooms", roomRoutes);
+
 router.get("/", async (req, res) => {
   res.json({ msg: "API route" });
 });
