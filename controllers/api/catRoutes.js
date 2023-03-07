618042d77793fd0298dd59f72c35203c6dd60a03
Ben L <veloswim@gmail.com>
2023-03-07 13:43:34 -0800

    wrapped cat routes in try/catch blocks

--- controllers/api/catRoutes.js
+++ controllers/api/catRoutes.js
@@ -3,29 +3,41 @@ const router = express.Router();
 const { User, Room, Cat } = require("../../models");
 
 router.get("/", async (req, res) => {
-  const allCats = await Cat.findAll();
+  try {
+    const allCats = await Cat.findAll();
 
-  res.json(allCats);
+    res.json(allCats);
+  } catch (err) {
+    res.status(500).json({ msg: "Error in the all cats route", err });
+  }
 });
 
 router.get("/:catId", async (req, res) => {
-  const foundCat = await Cat.findByPk(req.params.catId);
+  try {
+    const foundCat = await Cat.findByPk(req.params.catId);
 
-  if (!foundCat) {
-    res.status(404).json({ msg: "No such cat!" });
-  } else {
-    res.json(foundCat);
+    if (!foundCat) {
+      res.status(404).json({ msg: "No such cat!" });
+    } else {
+      res.json(foundCat);
+    }
+  } catch (err) {
+    res.status(500).json({ msg: "Error in get one cat route", err });
   }
 });
 
 router.post("/", async (req, res) => {
-  const createCat = await Cat.create({
-    cat_name: req.body.cat_name,
-    img_src: req.body.img_src,
-    min_work_time: req.body.min_work_time,
-  });
+  try {
+    const createCat = await Cat.create({
+      cat_name: req.body.cat_name,
+      img_src: req.body.img_src,
+      min_work_time: req.body.min_work_time,
+    });
 
-  res.json(createCat);
+    res.json(createCat);
+  } catch (err) {
+    res.status(500).json({ msg: "Error in the cat post route", err });
+  }
 });
 
 module.exports = router;
